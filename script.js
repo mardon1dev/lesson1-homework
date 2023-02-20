642c0b422068f62a02f4e315ae246e00c37a8a0f
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-02-21 01:17:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// 1-mashq
+
+// const ifYes = [], ifNo = [];
+
+// const findIndex = function (nums, num) {
+//     if (nums.indexOf(num) !== -1) {
+//         ifYes.push(nums.indexOf(num));
+//         console.log(ifYes);
+//     }else{
+//         console.log(ifNo);
+//     }
+// }
+// findIndex([1,2,3,4,5], 1);
+
+// 2-mashq
+
+// let people = [
+//     {name: "Alice", age:25},
+//     {name: "Bob", age:30},
+//     {name: "Charlie", age:35}
+// ]
+
+// const findAge = (person) => (person.age > 30) ? console.log("Big Boys"): console.log("Little Guys");
+
+// people.every(findAge); 
+
+// // const men = people.every((person) => person.age > 30 ? console.log("Big Boys") : console.log("Little guys"));
